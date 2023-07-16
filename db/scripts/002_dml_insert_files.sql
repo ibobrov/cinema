184441978fb6f3baa4cca8093af9dd4c8b638f4b
ibobrov <coderilya@gmail.com>
2023-07-17 05:25:04 +1000

    Added DB for prod and test, and sql scripts

---
+++ db/scripts/002_dml_insert_files.sql
@@ -0,0 +1,17 @@
+INSERT INTO files(name, path)
+VALUES('good_omens_2019.jpg', 'src/main/resources/files/good_omens_2019.jpg');
+
+INSERT INTO files(name, path)
+VALUES('once_upon_a_time_in_hollywood_2019.jpg', 'src/main/resources/files/once_upon_a_time_in_hollywood_2019.jpg');
+
+INSERT INTO files(name, path)
+VALUES('annihilation_2018.jpg', 'src/main/resources/files/annihilation_2018.jpg');
+
+INSERT INTO files(name, path)
+VALUES('blade_runner_2017.jpg', 'src/main/resources/files/blade_runner_2017.jpg');
+
+INSERT INTO files(name, path)
+VALUES('back_to_the_future_part_II_1989.jpg', 'src/main/resources/files/back_to_the_future_part_II_1989.jpg');
+
+INSERT INTO files(name, path)
+VALUES('jaws_1975.jpg', 'src/main/resources/files/jaws_1975.jpg');
