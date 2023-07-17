596a8824b90273cc142e53d240d4535768eeca25
ibobrov <coderilya@gmail.com>
2023-07-17 21:33:10 +1000

    Add dependency and properties for sqlo

---
+++ src/main/java/ru/job4j/cinema/configuration/DatasourceConfiguration.java
@@ -0,0 +1,31 @@
+package ru.job4j.cinema.configuration;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.sql2o.Sql2o;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatasourceConfiguration {
+
+    @Bean
+    public DataSource connectionPool(@Value("${datasource.url}") String url,
+                                     @Value("${datasource.username}") String username,
+                                     @Value("${datasource.password}") String password) {
+        return new BasicDataSource() {
+            {
+                setUrl(url);
+                setUsername(username);
+                setPassword(password);
+            }
+        };
+    }
+
+    @Bean
+    public Sql2o databaseClient(DataSource dataSource) {
+        return new Sql2o(dataSource);
+    }
+}
