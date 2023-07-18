eb53250f8cfe58e171c11ee6376fed6e21a7d309
ibobrov <coderilya@gmail.com>
2023-07-18 11:33:26 +1000

    Wrote dml scripts for bd

---
+++ db/scripts/005_dml_insert_halls.sql
@@ -0,0 +1,8 @@
+insert into halls(name, row_count, place_count, description)
+values('Hall 1', 20, 10, 'Large room with seats made of soft and comfortable upholstery.');
+
+insert into halls(name, row_count, place_count, description)
+values('Hall 2', 20, 10, 'Large room with seats made of soft and comfortable upholstery.');
+
+insert into halls(name, row_count, place_count, description)
+values('Vip room', 5, 5, 'VIP room with electric seats and air conditioning.');
