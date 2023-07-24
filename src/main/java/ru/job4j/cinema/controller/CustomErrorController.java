e560adb1bc787bbce864356707647526d9679db5
ibobrov <coderilya@gmail.com>
2023-07-24 18:52:41 +1000

    Added error handler and custom error pages

---
+++ src/main/java/ru/job4j/cinema/controller/CustomErrorController.java
@@ -0,0 +1,27 @@
+package ru.job4j.cinema.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String renderErrorPage(HttpServletRequest request) {
+        var status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+        if (status != null) {
+            var statusCode = Integer.parseInt(status.toString());
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "errors/error-404";
+            } else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "errors/error-500";
+            }
+        }
+        return "errors/error";
+    }
+}
